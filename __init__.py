27823f975d4caa9aced03504068984f2f7fdf41c
Evgeny Zislis <evgeny@anan.im>
2011-12-08 09:53:48 +0000

    first commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+MODULES = (
+    'boto',
+    'django',
+    'python-simpledb',
+    'beautifulsoup',
+    'facebook-python-sdk',
+)
+
+thirdparty_root = os.path.realpath(os.path.dirname(__file__))
+
+for mod in MODULES:
+    mod_path = os.path.join(thirdparty_root, mod)
+    if mod_path not in sys.path:
+        sys.path.insert(0, mod_path)
